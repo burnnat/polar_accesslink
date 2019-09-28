03edb77a2ff3c389ed23f4f85db45c00653a63d1
Nat Burns <nbaccount@burnskids.com>
2019-09-28 02:17:41 +0000

    Release 0.0.2.

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     packages=find_packages(exclude=['tests']),
     setup_requires=setup_requirements,
     url='https://github.com/burnnat/polar_accesslink',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
